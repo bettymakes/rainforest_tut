ebe82cd0cb15c20fc0d2c553ea3efc2c2d95fa58
blibotics <blibotics@gmail.com>
2013-07-10 14:14:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = '01a772d823b2911f02e7417d5a625104af603f0833d2f7168856a46075f8e02c8684966f5065947e2bd6f1ccff35f7de6a658f3049feb94175760a368ea936af'
