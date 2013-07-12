3143fc6dcf7b54b26a6d4dc71cd147cb9bd70777
bli <blisaur@blis-MacBook-Air.local>
2013-07-11 23:43:54 -0400

    Refactoring Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,15 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+    end
   end
 
   def new
-    @product = Product.new
+  @product = Product.new
   end
 
   def edit
@@ -14,29 +19,49 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @product }
+    end
+
   end
 
   def create
     @product = Product.new(params[:product])
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was sucessfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to products_path
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
   end
 
   def update
     @product = Product.find(params[:id])
-    if @product.update_attributes(params[:product])
-      redirect_to product_path(@product)
-    else
-      render :new
+
+      respond_to do |format|
+        if @product.update_attributes(params[:product])
+        format.html { redirect_to product_path(@product), notice: 'Product was successfully updated' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
