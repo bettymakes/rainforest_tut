d6f59e08ddda025696211196ad10df36fe74c952
blibotics <blibotics@gmail.com>
2013-07-14 03:21:49 -0400

    Creating reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rainforest::Application.routes.draw do
-  resources :products
+  get "reviews/show"
+
+  get "reviews/new"
+
+  get "reviews/edit"
+
+  resources :products do
+    resources :reviews, :except => [:index]
+  end
   resources :users, :only => [:new, :create]
   resources :sessions, :on => [:new, :create, :destroy]
 end
