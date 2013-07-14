9c62bdb9375fd01c5b42b086eb2f7fc28efc752b
blibotics <blibotics@gmail.com>
2013-07-14 03:31:58 -0400

    Updating actions in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,25 @@
 class ReviewsController < ApplicationController
   def show
+    @review = Review.find(params[:id])
   end
 
-  def new
+  def create
+    @review = @product.reviews.build(params[:review])
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to products_path, notice: "Review created successfully"
+    else
+      render :action => :show
+    end
   end
 
-  def edit
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def load_product
+    @product = Product.find(params[:product_id])
   end
 end
